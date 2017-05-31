43e09a2adf3edeaf4eda41554c3a227a49dec048
patrickb8man <patrickb8man@users.noreply.github.com>
2017-06-01 01:26:42 +1000

    Workaroudn for audio-related crashes

---
+++ PPeX/PPeXCore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPeX
+{
+    public static class PPeXCore
+    {
+        public static Settings Settings = Settings.Load();
+    }
+}
