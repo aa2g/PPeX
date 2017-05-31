c3f90e2f097e46a1b3d50170a88002cfd6feb1ff
Bepis <36346617+bbepis@users.noreply.github.com>
2017-05-31 23:58:52 +1000

    Changed the location of dependencies

--- PPeX/Manager.cs
+++ PPeX/Manager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,7 @@ namespace PPeX
 
         public static List<ExtendedArchive> archives = new List<ExtendedArchive>();
 
-        public static Settings Settings = Settings.Load();
+        public static Settings Settings;
 
 
         static Manager()
@@ -25,6 +26,27 @@ namespace PPeX
 #if DEBUG
             System.Diagnostics.Debugger.Launch();
 #endif
+
+            string dllsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"x86");//Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+            var assemblies = new List<Assembly>();
+
+            foreach (string path in new DirectoryInfo(dllsPath).GetFiles("*.dll").Select(x => x.FullName))
+            {
+                try
+                {
+                    assemblies.Add(Assembly.LoadFile(path));
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                return assemblies.First(x => args.Name == x.FullName);
+            };
+
+            Settings = Settings.Load();
 
             foreach (string dir in Directory.EnumerateFiles(Settings.PPXLocation, "*.ppx", SearchOption.TopDirectoryOnly).OrderBy(x => x))
             {
