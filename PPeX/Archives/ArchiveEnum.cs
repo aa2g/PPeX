0b2d8b008abe0a67ce56f716ac43dd85388708f2
Patrick Bateman <patrickb8man@users.noreply.github.com>
2017-08-08 20:41:22 +1000

    Preperation for using chunked data

--- PPeX/Archives/ArchiveEnum.cs
+++ PPeX/Archives/ArchiveEnum.cs
@@ -7,30 +7,9 @@ using System.Threading.Tasks;
 namespace PPeX
 {
     /// <summary>
-    /// The type of the PPX archive.
-    /// </summary>
-    public enum ArchiveType : ushort
-    {
-        /// <summary>
-        /// The archive is related to the base game.
-        /// </summary>
-        Archive = 1,
-
-        /// <summary>
-        /// The archive is related to a modification.
-        /// </summary>
-        Mod = 2,
-
-        /// <summary>
-        /// The archive is a custom music pack.
-        /// </summary>
-        BGMPack = 3
-    }
-
-    /// <summary>
     /// The data type of the archived file.
     /// </summary>
-    public enum ArchiveFileEncoding : byte
+    public enum ArchiveFileEncoding : ushort
     {
         /// <summary>
         /// The file is generic data and can be compressed normally.
@@ -62,26 +41,5 @@ namespace PPeX
         /// Zstd compression is used (see <see cref="Compressors.ZstdCompressor"/>)
         /// </summary>
         Zstandard = 2
-    }
-
-    /// <summary>
-    /// Metadata flags that are attached to archived files.
-    /// </summary>
-    [Flags]
-    public enum ArchiveFileFlags : byte
-    {
-#warning properly (re)implement this
-        /// <summary>
-        /// No metadata flags have been attached.
-        /// </summary>
-        None = 0,
-        /// <summary>
-        /// The file is related to the .ppx file and/or PPeX itself, and is not a file relating to AA2.
-        /// </summary>
-        Meta = 1,
-        /// <summary>
-        /// Indicates that the file should be forced into being kept in memory.
-        /// </summary>
-        MemoryCached = 2
     }
 }
---
+++ PPeX/Archives/ExtendedArchiveChunk.cs
@@ -0,0 +1,101 @@
+﻿using Crc32C;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPeX
+{
+    public class ExtendedArchiveChunk
+    {
+        public uint ID { get; protected set; }
+
+        public ArchiveFileCompression Compression { get; protected set; }
+
+        public uint CRC32C { get; protected set; }
+
+        public ulong Offset { get; protected set; }
+
+        public ulong CompressedLength { get; protected set; }
+
+        public ulong UncompressedLength { get; protected set; }
+
+        protected ExtendedArchive baseArchive;
+
+        protected ExtendedArchiveChunk()
+        {
+
+        }
+
+        public static ExtendedArchiveChunk ReadFromTable(BinaryReader reader, ExtendedArchive archive)
+        {
+            ExtendedArchiveChunk chunk = new ExtendedArchiveChunk();
+            chunk.baseArchive = archive;
+
+            chunk.ID = reader.ReadUInt32();
+            chunk.Compression = (ArchiveFileCompression)reader.ReadByte();
+            chunk.CRC32C = reader.ReadUInt32();
+            chunk.Offset = reader.ReadUInt64();
+            chunk.CompressedLength = reader.ReadUInt64();
+            chunk.UncompressedLength = reader.ReadUInt64();
+
+            return chunk;
+        }
+
+        protected IReadOnlyList<ArchiveFileSource> generateFileList()
+        {
+            List<ArchiveFileSource> files = new List<ArchiveFileSource>();
+
+            foreach (ArchiveFileSource file in baseArchive.Files)
+            {
+                if (file.ChunkID == ID)
+                    files.Add(file);
+            }
+
+            return files;
+        }
+
+        IReadOnlyList<ArchiveFileSource> _cachedFileList = null;
+
+        public IReadOnlyList<ArchiveFileSource> Files
+        {
+            get
+            {
+                if (_cachedFileList == null)
+                    _cachedFileList = generateFileList();
+
+                return _cachedFileList;
+            }
+        }
+
+        public Stream GetStream()
+        {
+            Stream stream = new FileStream(baseArchive.Filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            
+            return new Substream(stream, (long)Offset, (long)CompressedLength);
+        }
+
+        /// <summary>
+        /// Verifies the chunk data to the CRC32C checksum.
+        /// </summary>
+        /// <returns></returns>
+        public bool VerifyChecksum()
+        {
+            uint crc = 0;
+
+            using (Stream source = GetStream())
+            {
+                byte[] buffer = new byte[Core.Settings.BufferSize];
+                int length = 0;
+                while ((length = source.Read(buffer, 0, (int)Core.Settings.BufferSize)) > 0)
+                {
+                    crc = Crc32CAlgorithm.Append(crc, buffer, 0, length);
+                }
+            }
+
+            return crc == CRC32C;
+        }
+    }
+}
