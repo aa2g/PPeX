dbf6ed0f6aec7860942b6af479082d952497a36a
Bepis <36346617+bbepis@users.noreply.github.com>
2017-10-23 22:17:21 +1100

    Added extensive xx2 and xx tests

--- PPeXTests/Xx2Tests.cs
+++ PPeXTests/Xx2Tests.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static PPeXTests.xxCommon;
 
 namespace PPeXTests
 {
@@ -121,6 +122,256 @@ namespace PPeXTests
                 result = reader.ReadEncryptedString();
 
             Assert.AreEqual(testString, result);
+        }
+    }
+
+    [DeploymentItem("A00_00_00_03.xx")]
+    [TestClass()]
+    public class Xx2WriterTests
+    {
+        public static Xx2File File;
+        public static Xx2Writer x2writer = new Xx2Writer(0);
+        public static string Path = "A00_00_00_03.xx";
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            File = new Xx2File(new xxParser(new FileStream(Path, FileMode.Open)));
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            System.IO.File.Delete("A00_00_00_03.xx");
+        }
+
+
+        [TestMethod()]
+        public void BoneWritingTest()
+        {
+            var bones = File.RootObject.Children[0].Children[0].Children[0].Bones;
+
+            using (MemoryStream mem = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(mem))
+            using (BinaryReader reader = new BinaryReader(mem))
+            {
+                x2writer.WriteBones(writer, bones);
+
+                mem.Position = 0;
+
+                var newbones = Xx2Reader.ReadBones(reader, bones.Count);
+
+                VerifyBones(bones, newbones);
+            }
+        }
+
+        [TestMethod()]
+        public void VertexWritingTest()
+        {
+            var verticies = File.RootObject.Children[0].Children[0].Children[0].Meshes[0].Verticies;
+
+            using (MemoryStream mem = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(mem))
+            using (BinaryReader reader = new BinaryReader(mem))
+            {
+                x2writer.WriteVerticies(writer, verticies);
+
+                mem.Position = 0;
+
+                var newverticies = Xx2Reader.ReadVerticies(reader, verticies.Count);
+
+                VerifyVerticies(verticies, newverticies);
+            }
+        }
+
+        [TestMethod()]
+        public void MeshWritingTest()
+        {
+            var meshes = File.RootObject.Children[0].Children[0].Children[0].Meshes;
+
+            using (MemoryStream mem = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(mem))
+            using (BinaryReader reader = new BinaryReader(mem))
+            {
+                x2writer.WriteMesh(writer, meshes[0]);
+
+                mem.Position = 0;
+
+                var newmesh = Xx2Reader.ReadMesh(reader);
+
+                VerifyMesh(meshes[0], newmesh);
+            }
+        }
+
+        [TestMethod()]
+        public void ObjectWritingTest()
+        {
+            var obj = File.RootObject;
+
+            using (MemoryStream mem = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(mem))
+            using (BinaryReader reader = new BinaryReader(mem))
+            {
+                x2writer.WriteObject(writer, obj);
+
+                mem.Position = 0;
+
+                var newobj = Xx2Reader.ReadObject(reader);
+
+                VerifyObject(obj, newobj);
+            }
+        }
+
+        [TestMethod()]
+        public void FileWritingTest()
+        {
+            byte[] data = System.IO.File.ReadAllBytes(Path);
+
+            using (MemoryStream mem = new MemoryStream())
+            using (MemoryStream write = new MemoryStream())
+            {
+                new Xx2Writer(0).Write(File, write);
+
+                write.Position = 0;
+
+                var xxfile2 = Xx2Reader.Read(write);
+
+                VerifyFile(File, xxfile2);
+            }
+        }
+    }
+
+    [DeploymentItem("A00_00_00_03.xx")]
+    [TestClass()]
+    public class xxParserTests
+    {
+        public static Xx2File File;
+        public static Xx2Writer x2writer = new Xx2Writer(0);
+        public static string Path = "A00_00_00_03.xx";
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            File = new Xx2File(new xxParser(new FileStream(Path, FileMode.Open)));
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            System.IO.File.Delete("A00_00_00_03.xx");
+        }
+
+
+        [TestMethod()]
+        public void BoneWritingTest()
+        {
+            var bones = File.RootObject.Children[0].Children[0].Children[0].Bones;
+
+            using (MemoryStream mem = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(mem))
+            using (BinaryReader reader = new BinaryReader(mem))
+            {
+                bones[0].Write(writer);
+
+                mem.Position = 0;
+
+                var newbone = new xxBone(reader);
+
+                VerifyBones(new List<xxBone> { bones[0] }, new List<xxBone> { newbone });
+            }
+        }
+
+        [TestMethod()]
+        public void VertexWritingTest()
+        {
+            var verticies = File.RootObject.Children[0].Children[0].Children[0].Meshes[0].Verticies;
+
+            using (MemoryStream mem = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(mem))
+            using (BinaryReader reader = new BinaryReader(mem))
+            {
+                verticies[0].Write(writer);
+
+                mem.Position = 0;
+
+                var newvertex = new xxVertex(reader, File.Version);
+
+                VerifyVerticies(new List<xxVertex> { verticies[0] }, new List<xxVertex> { newvertex });
+            }
+        }
+
+        [TestMethod()]
+        public void MeshWritingTest()
+        {
+            var meshes = File.RootObject.Children[0].Children[0].Children[0].Meshes;
+
+            using (MemoryStream mem = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(mem))
+            using (BinaryReader reader = new BinaryReader(mem))
+            {
+                meshes[0].Write(writer, File.Version);
+
+                mem.Position = 0;
+
+                var newmesh = new xxMeshInfo(reader, File.Version, 0);
+
+                VerifyMesh(meshes[0], newmesh);
+            }
+        }
+
+        [TestMethod()]
+        public void ObjectWritingTest()
+        {
+            var obj = File.RootObject;
+
+            using (MemoryStream mem = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(mem))
+            using (BinaryReader reader = new BinaryReader(mem))
+            {
+                obj.Write(writer, File.Version);
+
+                mem.Position = 0;
+
+                var newobj = new xxObject(reader, File.Version);
+
+                VerifyObject(obj, newobj);
+            }
+        }
+
+        [TestMethod()]
+        public void FileWritingTest()
+        {
+            byte[] data = System.IO.File.ReadAllBytes(Path);
+
+            using (MemoryStream mem = new MemoryStream())
+            using (MemoryStream write = new MemoryStream())
+            {
+                File.DecodeToXX(mem);
+
+                mem.Position = 0;
+
+                xxParser parser = new xxParser(mem);
+
+                VerifyFile(File, new Xx2File(parser));
+            }
+        }
+
+        [TestMethod()]
+        public void BinaryFileWritingTest()
+        {
+            byte[] data = System.IO.File.ReadAllBytes(Path);
+
+            using (MemoryStream mem = new MemoryStream())
+            using (MemoryStream write = new MemoryStream())
+            {
+                File.DecodeToXX(mem);
+
+                mem.Position = 0;
+
+                xxParser parser = new xxParser(mem);
+
+                Assert.IsTrue(PPeX.Utility.CompareBytes(data, mem.ToArray()));
+            }
         }
     }
 }
---
+++ PPeXTests/xxCommon.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PPeX.Xx2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPeXTests
+{
+    public static class xxCommon
+    {
+        public static void VerifyFaces(IList<xxFace> original, IList<xxFace> saved)
+        {
+            Assert.AreEqual(original.Count, saved.Count, "Face count does not match.");
+
+            for (int i = 0; i < original.Count; i++)
+            {
+                Assert.AreEqual(original[i].VertexIndicies.Length, saved[i].VertexIndicies.Length, "Face vertex count does not match.");
+
+                for (int x = 0; x < original[i].VertexIndicies.Length; x++)
+                    Assert.AreEqual(original[i].VertexIndicies[x], saved[i].VertexIndicies[x], "Face verticies do not match.");
+            }
+        }
+
+        public static void VerifyBones(IList<xxBone> original, IList<xxBone> saved)
+        {
+            Assert.AreEqual(original.Count, saved.Count, "Bone count does not match.");
+
+            for (int i = 0; i < original.Count; i++)
+            {
+                Assert.AreEqual(original[i].Name, saved[i].Name, "Bone name does not match.");
+
+                Assert.AreEqual(original[i].Index, saved[i].Index, "Bone index does not match.");
+
+                for (int x = 0; x < 4; x++)
+                    for (int y = 0; y < 4; y++)
+                        Assert.AreEqual(original[i].Transforms[x, y], saved[i].Transforms[x, y], "Bone transforms do not match.");
+            }
+        }
+
+        public static void VerifyUnknowns(IList<byte[]> original, IList<byte[]> saved)
+        {
+            Assert.AreEqual(original.Count, saved.Count, "Unknown count does not match.");
+
+            for (int i = 0; i < original.Count; i++)
+            {
+                Assert.AreEqual(original[i].Length, saved[i].Length, "Unknown length does not match.");
+
+                for (int x = 0; x < original[i].Length; x++)
+                    Assert.AreEqual(original[i][x], saved[i][x], "Unknown does not match.");
+            }
+        }
+
+        public static void VerifyVerticies(IList<xxVertex> original, IList<xxVertex> saved)
+        {
+            Assert.AreEqual(original.Count, saved.Count, "Vertex count does not match.");
+
+            for (int i = 0; i < original.Count; i++)
+            {
+                Assert.AreEqual(original[i].isIndexUInt16, saved[i].isIndexUInt16, "Vertex index size does not match.");
+
+                Assert.AreEqual(original[i].Index, saved[i].Index, "Vertex index does not match.");
+
+                //verify positions
+                Assert.AreEqual(original[i].Position.Length, saved[i].Position.Length, "Vertex position count does not match.");
+
+                for (int x = 0; x < original[i].Position.Length; x++)
+                    Assert.AreEqual(original[i].Position[x], saved[i].Position[x], "Vertex positions do not match.");
+
+                //verify normals
+                Assert.AreEqual(original[i].Normal.Length, saved[i].Normal.Length, "Vertex normal count does not match.");
+
+                for (int x = 0; x < original[i].Normal.Length; x++)
+                    Assert.AreEqual(original[i].Normal[x], saved[i].Normal[x], "Vertex normals do not match.");
+
+                //verify weights
+                Assert.AreEqual(original[i].Weights.Length, saved[i].Weights.Length, "Vertex weight count does not match.");
+
+                for (int x = 0; x < original[i].Weights.Length; x++)
+                    Assert.AreEqual(original[i].Weights[x], saved[i].Weights[x], "Vertex weights do not match.");
+
+                //verify UVs
+                Assert.AreEqual(original[i].UV.Length, saved[i].UV.Length, "Vertex UV count does not match.");
+
+                for (int x = 0; x < original[i].UV.Length; x++)
+                    Assert.AreEqual(original[i].UV[x], saved[i].UV[x], "Vertex UVs do not match.");
+
+                //verify bone indicies
+                Assert.AreEqual(original[i].BoneIndicies.Length, saved[i].BoneIndicies.Length, "Vertex bone index count does not match.");
+
+                for (int x = 0; x < original[i].BoneIndicies.Length; x++)
+                    Assert.AreEqual(original[i].BoneIndicies[x], saved[i].BoneIndicies[x], "Vertex bone indicies do not match.");
+
+                //verify unknown
+                Assert.AreEqual(original[i].Unknown.Length, saved[i].Unknown.Length, "Vertex unknown length does not match.");
+
+                for (int x = 0; x < original[i].Unknown.Length; x++)
+                    Assert.AreEqual(original[i].Unknown[x], saved[i].Unknown[x], "Vertex unknown does not match.");
+            }
+        }
+
+        public static void VerifyMesh(xxMeshInfo original, xxMeshInfo saved)
+        {
+            //verify unknowns
+            VerifyUnknowns(original.Unknowns, saved.Unknowns);
+
+            //verify faces
+            VerifyFaces(original.Faces, saved.Faces);
+
+            //verify verticies
+            VerifyVerticies(original.Verticies, saved.Verticies);
+        }
+
+        public static void VerifyObject(xxObject original, xxObject saved)
+        {
+            //verify unknowns
+            VerifyUnknowns(original.Unknowns, saved.Unknowns);
+
+            //verify name
+            Assert.AreEqual(original.Name, saved.Name, "Object name does not match.");
+
+            //verify bones
+            VerifyBones(original.Bones, saved.Bones);
+
+            //verify transforms
+            for (int x = 0; x < 4; x++)
+                for (int y = 0; y < 4; y++)
+                    Assert.AreEqual(original.Transforms[x, y], saved.Transforms[x, y], "Object transforms do not match.");
+
+            //verify meshes
+            Assert.AreEqual(original.Meshes.Count, saved.Meshes.Count, "Object mesh count does not match.");
+
+            for (int i = 0; i < original.Meshes.Count; i++)
+            {
+                VerifyMesh(original.Meshes[i], saved.Meshes[i]);
+            }
+
+            //verify duplicate verticies
+            VerifyVerticies(original.DuplicateVerticies, saved.DuplicateVerticies);
+
+            //verify children
+            Assert.AreEqual(original.Meshes.Count, saved.Meshes.Count, "Object child count does not match.");
+
+            for (int i = 0; i < original.Meshes.Count; i++)
+            {
+                VerifyObject(original.Children[i], saved.Children[i]);
+            }
+        }
+
+        public static void VerifyFile(Xx2File original, Xx2File saved)
+        {
+            //verify version
+            Assert.AreEqual(original.Version, saved.Version, "Version does not match.");
+
+            //verify unknown
+            Assert.AreEqual(original.Unknown.Length, saved.Unknown.Length, "Unknown length does not match.");
+
+            for (int x = 0; x < original.Unknown.Length; x++)
+                Assert.AreEqual(original.Unknown[x], saved.Unknown[x], "Unknown does not match.");
+
+            //verify object
+            VerifyObject(original.RootObject, saved.RootObject);
+
+            //verify unencoded data
+            Assert.AreEqual(original.UnencodedData.Length, saved.UnencodedData.Length, "Unencoded data length does not match.");
+
+            for (int x = 0; x < original.UnencodedData.Length; x++)
+                Assert.AreEqual(original.UnencodedData[x], saved.UnencodedData[x], "Unencoded data does not match.");
+        }
+    }
+}
