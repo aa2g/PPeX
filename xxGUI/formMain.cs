afc1736704032ef90fcb6c03c9e0968f59eea01b
patrickb8man <patrickb8man@users.noreply.github.com>
2017-10-25 00:03:39 +1100

    Updated xxGUI to show textures

--- xxGUI/formMain.cs
+++ xxGUI/formMain.cs
@@ -19,7 +19,7 @@ namespace xxGUI
             InitializeComponent();
         }
 
-        string original = @"B:\A00_39_02_04.xx"; //@"B:\A00_00_01_01.xx";
+        string original = @"B:\A00_00_00_00.xx"; //@"B:\A00_00_01_01.xx"; 
 
         xxParser parser;
 
@@ -28,6 +28,21 @@ namespace xxGUI
             parser = new xxParser(new FileStream(original, FileMode.Open));
 
             trvObjects.Nodes.Add(makeNode(parser.RootObject));
+
+            foreach (var texture in parser.Textures)
+            {
+                var item = lsvTextures.Items.Add(texture.Name);
+
+                item.SubItems.Add(texture.Width + "x" + texture.Height);
+
+                //item.SubItems.Add(texture.Checksum.ToString("X2"));
+                using (MemoryStream mem = new MemoryStream(texture.ImageData))
+                    item.SubItems.Add(BitConverter.ToString(PPeX.Utility.GetMd5(mem)));
+
+                item.SubItems.Add(PPeX.Utility.GetBytesReadable(texture.ImageData.Length));
+
+                item.Tag = texture.ImageData;
+            }
         }
 
         TreeNode makeNode(xxObject obj)
@@ -44,7 +59,7 @@ namespace xxGUI
         {
             Xx2File file = new Xx2File(parser);
 
-            Xx2Writer writer = new Xx2Writer();
+            Xx2Writer writer = new Xx2Writer(16);
             writer.Write(file, @"B:\test.xx2");
 
             ZstdNet.Compressor originalcomp = new ZstdNet.Compressor(new ZstdNet.CompressionOptions(22));
@@ -69,6 +84,98 @@ namespace xxGUI
                     return false;
             }
             return true;
+        }
+
+        private void OpenXX2_Click(object sender, EventArgs e)
+        {
+            trvObjects.Nodes.Clear();
+
+            Xx2File file = Xx2Reader.Read(@"B:\test.xx2");
+
+            trvObjects.Nodes.Add(makeNode(file.RootObject));
+        }
+
+        private void btnSaveXX_Click(object sender, EventArgs e)
+        {
+            Xx2File file = new Xx2File(parser);
+
+            using (FileStream fs = new FileStream("B:\\test.xx", FileMode.Create))
+                file.DecodeToXX(fs);
+        }
+
+        private void btnScanFolder_Click(object sender, EventArgs e)
+        {
+            foreach (string file in Directory.EnumerateFiles(@"I:\Artificial Academy 2\patching\jg2e01_00_00\", "*.xx"))
+            {
+                byte[] data = File.ReadAllBytes(file);
+
+                var xxfile = new Xx2File(new xxParser(new FileStream(file, FileMode.Open)));
+
+                using (MemoryStream mem = new MemoryStream())
+                using (MemoryStream write = new MemoryStream())
+                {
+                    new Xx2Writer(0).Write(xxfile, write);
+
+                    write.Position = 0;
+
+                    var xxfile2 = Xx2Reader.Read(write);
+
+                    xxfile2.DecodeToXX(mem);
+
+                    File.WriteAllBytes("B:\\data1.xx", data);
+                    File.WriteAllBytes("B:\\data2.xx", mem.ToArray());
+
+                    if (!PPeX.Utility.CompareBytes(data, mem.ToArray()))
+                        MessageBox.Show(file);
+                }
+            }
+        }
+
+        private void lsvTextures_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lsvTextures.SelectedItems.Count == 1)
+            {
+                var item = lsvTextures.SelectedItems[0];
+
+                byte[] data = (item.Tag as byte[]);
+
+                ImageMagick.MagickImage mimage;
+
+                if (item.Text.EndsWith(".bmp"))
+                {
+                    data[0] = (byte)'B';
+                    data[1] = (byte)'M';
+
+                    mimage = new ImageMagick.MagickImage(data);
+                }
+                else
+                {
+                    string path = "B:\\" + item.Text;
+
+                    File.WriteAllBytes(path, data);
+
+                    mimage = new ImageMagick.MagickImage(path);
+
+                    File.Delete(path);
+                }
+                
+
+                Bitmap image;
+
+                using (var raw = mimage.ToBitmap())
+                    image = new Bitmap(raw);
+
+                //var image = Image.FromStream(new MemoryStream(data));
+
+                if (imgTexture.Image != null)
+                {
+                    imgTexture.Image.Dispose();
+                }
+
+                imgTexture.Image = image;
+
+                mimage.Dispose();
+            }
         }
     }
 }
