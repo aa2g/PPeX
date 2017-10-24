b9c442cbd07b2599ff3f83edac89b11eda4449fa
patrickb8man <patrickb8man@users.noreply.github.com>
2017-10-25 00:02:13 +1100

    Updated Xx2 to handle textures and materials

---
+++ PPeX.Xx2/XX/xxTexture.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPeX.Xx2
+{
+    public class xxTexture
+    {
+        public string Name;
+
+        public byte[] Unknown;
+
+        public int Width;
+
+        public int Height;
+
+        public int Depth;
+
+        public int MipLevels;
+
+        public int Format;
+
+        public int ResourceType;
+
+        public int ImageFileFormat;
+
+        public byte Checksum;
+
+        public byte[] ImageData;
+
+        public xxTexture(BinaryReader reader)
+        {
+            Name = reader.ReadEncryptedString();
+
+            Unknown = reader.ReadBytes(4);
+
+            Width = reader.ReadInt32();
+            Height = reader.ReadInt32();
+            Depth = reader.ReadInt32();
+            MipLevels = reader.ReadInt32();
+            Format = reader.ReadInt32();
+            ResourceType = reader.ReadInt32();
+            ImageFileFormat = reader.ReadInt32();
+            Checksum = reader.ReadByte();
+
+            int length = reader.ReadInt32();
+
+            ImageData = reader.ReadBytes(length);
+        }
+
+        internal xxTexture()
+        {
+
+        }
+
+        public void Write(BinaryWriter writer)
+        {
+            writer.WriteEncryptedString(Name);
+
+            writer.Write(Unknown);
+
+            writer.Write(Width);
+            writer.Write(Height);
+            writer.Write(Depth);
+            writer.Write(MipLevels);
+            writer.Write(Format);
+            writer.Write(ResourceType);
+            writer.Write(ImageFileFormat);
+            writer.Write(Checksum);
+
+            writer.Write((int)ImageData.Length);
+
+            writer.Write(ImageData);
+        }
+    }
+}
--- PPeX.Xx2/Xx2Writer.cs
+++ PPeX.Xx2/Xx2Writer.cs
@@ -34,12 +34,26 @@ namespace PPeX.Xx2
                 writer.Write(file.Version);
 
 
-                writer.Write(file.Unknown.Length);
+                writer.Write(file.HeaderUnknown.Length);
 
-                writer.Write(file.Unknown);
+                writer.Write(file.HeaderUnknown);
 
 
                 WriteObject(writer, file.RootObject);
+
+
+                //always length of 4
+                writer.Write(file.MaterialUnknown);
+
+
+                writer.Write(file.Materials.Count);
+
+                WriteMaterials(writer, file.Materials);
+
+
+                writer.Write(file.Textures.Count);
+
+                WriteTextures(writer, file.Textures);
 
 
                 writer.Write(file.UnencodedData.Length);
@@ -198,6 +212,107 @@ namespace PPeX.Xx2
                 {
                     writer.Write(FloatEncoder.Encode(bones.Select(b => b.Transforms[x, y]).ToArray()));
                 }
+        }
+
+        public void WriteMaterials(BinaryWriter writer, List<xxMaterial> materials)
+        {
+            if (materials.Count == 0)
+                return;
+
+            //we can't really strategically encode much here
+            int unknownSize = materials[0].Unknown.Length;
+
+            //encode universal unknown size
+            writer.Write(unknownSize);
+            
+            for (int i = 0; i < materials.Count; i++)
+            {
+                //write name
+                writer.Write(materials[i].Name);
+
+                //write color sets
+                materials[i].Ambient.Write(writer);
+                materials[i].Diffuse.Write(writer);
+                materials[i].Emissive.Write(writer);
+                materials[i].Specular.Write(writer);
+
+                //write power
+                writer.Write(materials[i].Power);
+
+                //write material textures
+                //*always* in groups of 4
+                for (int x = 0; x < 4; x++)
+                {
+                    var materialTexture = materials[i].Textures[x];
+
+                    writer.Write(materialTexture.Name);
+
+                    //always a size of 16
+                    writer.Write(materialTexture.Unknown);
+                }
+
+                //write universally sized unknown
+                writer.Write(materials[i].Unknown);
+            }
+        }
+
+        public void WriteTextures(BinaryWriter writer, List<xxTexture> textures)
+        {
+            if (textures.Count == 0)
+                return;
+
+            //time to get tricky
+
+            List<Tuple<byte[], byte[]>> checksums = new List<Tuple<byte[], byte[]>>();
+
+            List<int> indexedChecksums = new List<int>();
+
+            for (int i = 0; i < textures.Count; i++)
+            {
+                byte[] checksum = Utility.GetMd5(textures[i].ImageData);
+
+                if (!checksums.Any(x => Utility.CompareBytes(x.Item1, checksum)))
+                    checksums.Add(new Tuple<byte[], byte[]>(checksum, textures[i].ImageData));
+
+                //find index
+                indexedChecksums.Add(checksums.FindIndex((x) => Utility.CompareBytes(x.Item1, checksum)));
+            }
+
+
+            //write each texture metadata
+            for (int i = 0; i < textures.Count; i++)
+            {
+                var texture = textures[i];
+
+                writer.Write(texture.Name);
+
+                writer.Write(texture.Checksum);
+                writer.Write(texture.Depth);
+                writer.Write(texture.Format);
+                writer.Write(texture.Height);
+                writer.Write(texture.ImageFileFormat);
+                writer.Write(texture.MipLevels);
+                writer.Write(texture.ResourceType);
+                writer.Write(texture.Width);
+
+                //unknown is always 4 bytes long
+                writer.Write(texture.Unknown);
+
+                //write the data index
+                writer.Write(indexedChecksums[i]);
+            }
+
+            //write each individual texture data
+            writer.Write(checksums.Count);
+
+            for (int i = 0; i < checksums.Count; i++)
+            {
+                byte[] data = checksums[i].Item2;
+
+                writer.Write(data.Length);
+
+                writer.Write(data);
+            }
         }
     }
 }
