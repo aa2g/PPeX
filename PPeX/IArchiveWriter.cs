f078e94ab0490f58b278ee7d856553ad067e442f
patrickb8man <patrickb8man@users.noreply.github.com>
2017-11-16 00:23:39 +1100

    Added an archive chunk appender + metadata editor

---
+++ PPeX/IArchiveWriter.cs
@@ -0,0 +1,14 @@
+﻿using PPeX.Xx2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPeX
+{
+    public interface IArchiveWriter
+    {
+        TextureBank TextureBank { get; }
+    }
+}
