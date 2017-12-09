be4d1d001d54693f592fbfc9d2351855df79b551
patrickb8man <patrickb8man@users.noreply.github.com>
2017-12-10 00:15:26 +1100

    Added multi-threaded audio encoding

---
+++ PPeX/Archives/Writers/HybridEncoder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPeX.Archives.Writers
+{
+    public class HybridEncoder : IThreadWork
+    {
+        internal HybridChunkWriter chunkWriter;
+        protected IEnumerable<ISubfile> files;
+
+        public HybridEncoder(uint ID, IEnumerable<ISubfile> files, IArchiveContainer writer)
+        {
+            chunkWriter = new HybridChunkWriter(ID, ArchiveChunkCompression.Uncompressed, ChunkType.Generic, writer);
+
+            this.files = files;
+        }
+
+        public ChunkReceipt Receipt => chunkWriter.Receipt;
+
+        public void Dispose()
+        {
+            chunkWriter.Dispose();
+        }
+
+        public Stream GetData()
+        {
+            foreach (ISubfile file in files)
+                chunkWriter.AddFile(file);
+
+            chunkWriter.Compress();
+            return chunkWriter.CompressedStream;
+        }
+    }
+}
