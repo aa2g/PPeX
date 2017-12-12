23860a825873c6babe3bae0f427b649fcdd7e3e0
Bepis <36346617+bbepis@users.noreply.github.com>
2017-12-12 22:41:42 +1100

    Added a crash handler for PPeXUI

---
+++ PPeXUI/formError.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PPeXUI
+{
+    partial class formError
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lsbFiles = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtException = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PPeXUI.Properties.Resources.ppexerror;
+            this.pictureBox1.Location = new System.Drawing.Point(330, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(161, 156);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 61);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PPeXUI has encounted an unexpected error and crashed. \r\n\r\nThe following files hav" +
+    "e been generated and would be useful to submit with an error report:\r\n";
+            // 
+            // lsbFiles
+            // 
+            this.lsbFiles.FormattingEnabled = true;
+            this.lsbFiles.Location = new System.Drawing.Point(12, 76);
+            this.lsbFiles.Name = "lsbFiles";
+            this.lsbFiles.Size = new System.Drawing.Size(303, 69);
+            this.lsbFiles.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(303, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Additional information:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // txtException
+            // 
+            this.txtException.BackColor = System.Drawing.SystemColors.Window;
+            this.txtException.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtException.Location = new System.Drawing.Point(12, 174);
+            this.txtException.Multiline = true;
+            this.txtException.Name = "txtException";
+            this.txtException.ReadOnly = true;
+            this.txtException.Size = new System.Drawing.Size(479, 84);
+            this.txtException.TabIndex = 4;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(12, 264);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(479, 23);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // formError
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 296);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtException);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lsbFiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formError";
+            this.Text = "Unhandled Exception";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lsbFiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtException;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
---
+++ PPeXUI/formError.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPeXUI
+{
+    public partial class formError : Form
+    {
+        public formError()
+        {
+            InitializeComponent();
+        }
+
+        public formError(string exception, string[] files)
+        {
+            InitializeComponent();
+
+            txtException.Text = exception;
+
+            foreach (string file in files)
+                lsbFiles.Items.Add(file);
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
