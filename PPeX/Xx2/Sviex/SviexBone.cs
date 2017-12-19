6404ced4817d7b7bbf61d8c9a1d3b29738d3cb4f
Patrick Bateman <patrickb8man@users.noreply.github.com>
2017-12-19 18:23:19 +1100

    Added a sviex parser

---
+++ PPeX/Xx2/Sviex/SviexBone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPeX.Xx2.Sviex
+{
+    public class SviexBone
+    {
+        public string Name;
+        public int Index;
+        public float[] Transforms = new float[16];
+
+        public static SviexBone FromReader(BinaryReader reader)
+        {
+            SviexBone bone = new SviexBone();
+
+            bone.Name = reader.ReadEncryptedString();
+            bone.Index = reader.ReadInt32();
+            
+            for (int i = 0; i < 16; i++)
+                bone.Transforms[i] = reader.ReadSingle();
+
+            return bone;
+        }
+
+        public void Write(BinaryWriter writer)
+        {
+            writer.WriteEncryptedString(Name);
+            writer.Write(Index);
+
+            for (int i = 0; i < 16; i++)
+                writer.Write(Transforms[i]);
+        }
+    }
+}
