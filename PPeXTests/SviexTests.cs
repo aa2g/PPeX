d0cb62229b25ae64fffb2bb68fd53a869c1b325f
Bepis <36346617+bbepis@users.noreply.github.com>
2017-12-19 18:23:19 +1100

    Added a sviex parser

---
+++ PPeXTests/SviexTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PPeX.Xx2.Sviex;
+
+namespace PPeXTests
+{
+    [DeploymentItem("A00_01_O_blazer_S.sviex")]
+    [TestClass]
+    public class SviexTests
+    {
+        public static readonly string Path = "A00_01_O_blazer_S.sviex";
+
+        [TestMethod()]
+        public void BinaryFileWritingTest()
+        {
+            byte[] data = File.ReadAllBytes(Path);
+
+            using (MemoryStream mem = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(mem))
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(Path)))
+            {
+                SviexFile file = SviexFile.FromReader(reader);
+
+                file.Write(writer);
+
+                byte[] encoded = mem.ToArray();
+
+                Assert.IsTrue(PPeX.Utility.CompareBytes(data, encoded));
+            }
+        }
+    }
+}
