b218d5edda71238f93670259cbd5802f7416656c
Bepis <36346617+bbepis@users.noreply.github.com>
2018-01-18 09:04:21 +1100

    Reverse load order of archives

--- PPeXM64/Program.cs
+++ PPeXM64/Program.cs
@@ -71,7 +71,7 @@ namespace PPeXM64
                 List<ExtendedArchive> ArchivesToLoad = new List<ExtendedArchive>();
 
                 //Index all .ppx files in the location
-                foreach (string arc in Directory.EnumerateFiles(Core.Settings.PPXLocation, "*.ppx", SearchOption.TopDirectoryOnly).OrderBy(x => x).Reverse())
+                foreach (string arc in Directory.EnumerateFiles(Core.Settings.PPXLocation, "*.ppx", SearchOption.TopDirectoryOnly).OrderBy(x => x))
                 {
                     var archive = new ExtendedArchive(arc);
 
